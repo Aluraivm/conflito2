e1d4c15744c23d64b8026126a1e6cf2eea4df097
IgorVicente <igor_machado05@hotmail.com>
2022-04-28 21:51:58 -0300

    initial

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,5 +8,17 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
         }
+        private static void rezabrabaa(int a, int b, int c)
+        {
+            int[] array = new int[3] { a, b, c };
+            print(array);
+        }
+        private static void print(int[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
     }
 }
